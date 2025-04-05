779f4be443073ce869096ff78416b0dd1e277aec
matkv <mail@matkv.dev>
2025-04-05 16:52:14 +0200

    styling improvements for the bookmarks page

--- src/lib/components/Bookmarks.svelte
+++ src/lib/components/Bookmarks.svelte
@@ -1,4 +1,4 @@
-<div class="container mx-auto p-6">
+<div class="container mx-auto p-2">
     <!-- FOR NOW, THIS IS HARDCODED, SHOULD BE POSSIBLE TO
      CREATE AND INSERT BOOKSMARKS ON AN ADMIN PAGE THAT ACCESSES THE DB -->
     <section class="mb-8">
@@ -8,28 +8,46 @@
         >
             <a
                 href="https://teuxdeux.com/home"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">TeuxDeux</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     The to-do list app for organizing everything.
                 </p>
             </a>
             <a
                 href="https://matkv.dev"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">matkv.dev</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Personal website of a software developer from Austria.
                 </p>
             </a>
             <a
                 href="https://code.matkv.dev"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">code.matkv.dev</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
+                    Repository and projects of matkv.dev.
+                </p>
+            </a>
+            <a
+                href="https://code.matkv.dev"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
+            >
+                <h3 class="text-lg font-semibold">code.matkv.dev</h3>
+                <p class="text-sm text-gray-300">
+                    Repository and projects of matkv.dev.
+                </p>
+            </a>
+            <a
+                href="https://code.matkv.dev"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
+            >
+                <h3 class="text-lg font-semibold">code.matkv.dev</h3>
+                <p class="text-sm text-gray-300">
                     Repository and projects of matkv.dev.
                 </p>
             </a>
@@ -43,28 +61,28 @@
         >
             <a
                 href="https://mail.google.com"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Mail</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Gmail is a free email service from Google.
                 </p>
             </a>
             <a
                 href="https://calendar.google.com"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Calendar</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Google Calendar helps you stay on top of your plans.
                 </p>
             </a>
             <a
                 href="https://drive.google.com/drive/my-drive"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Drive</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Google Drive is a cloud-based storage service that lets you
                     upload, store, and share files.
                 </p>
@@ -79,30 +97,30 @@
         >
             <a
                 href="https://stackoverflow.com/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Stack Overflow</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     A community for sharing and discussing new ideas, work, and
                     news stories in programming.
                 </p>
             </a>
             <a
                 href="https://news.ycombinator.com/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Hacker News</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     A community for sharing and discussing new ideas, work, and
                     news stories.
                 </p>
             </a>
             <a
                 href="https://boards.4channel.org/g/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/g/</h3>
-                <p class="text-sm text-gray-600">4chan's technology board.</p>
+                <p class="text-sm text-gray-300">4chan's technology board.</p>
             </a>
         </div>
     </section>
@@ -114,26 +132,26 @@
         >
             <a
                 href="https://www.twitch.tv/directory/following/live"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Twitch</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     The world's leading video platform and community for gamers.
                 </p>
             </a>
             <a
                 href="https://lichess.org/@/matkv"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Lichess</h3>
-                <p class="text-sm text-gray-600">Free online chess.</p>
+                <p class="text-sm text-gray-300">Free online chess.</p>
             </a>
             <a
                 href="https://boards.4channel.org/wsg/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/wsg/</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     4chan's work-safe GIF board.
                 </p>
             </a>
@@ -147,28 +165,28 @@
         >
             <a
                 href="https://www.goodreads.com"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Goodreads</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Meet your next favorite book.
                 </p>
             </a>
             <a
                 href="https://www.reddit.com/user/matkv/m/books/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/r/books</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Subreddit for book discussions and recommendations.
                 </p>
             </a>
             <a
                 href="https://boards.4channel.org/lit/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/lit/</h3>
-                <p class="text-sm text-gray-600">4chan's literature board.</p>
+                <p class="text-sm text-gray-300">4chan's literature board.</p>
             </a>
         </div>
     </section>
@@ -180,29 +198,29 @@
         >
             <a
                 href="https://www.imdb.com"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">IMDb</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     The world's most popular and authoritative source for movie,
                     TV, and celebrity content.
                 </p>
             </a>
             <a
                 href="https://boards.4channel.org/tv/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/tv/</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     4chan's television and film board.
                 </p>
             </a>
             <a
                 href="https://www.reddit.com/user/matkv/m/movies/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/r/movies</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Subreddit for movie discussions and recommendations.
                 </p>
             </a>
@@ -216,30 +234,30 @@
         >
             <a
                 href="https://open.spotify.com/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Spotify</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Digital music service that gives you access to millions of
                     songs.
                 </p>
             </a>
             <a
                 href="https://www.ultimate-guitar.com/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Ultimate Guitar</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Your #1 source for chords, guitar tabs, bass tabs, ukulele
                     chords, and more.
                 </p>
             </a>
             <a
                 href="https://boards.4channel.org/mu/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/mu/</h3>
-                <p class="text-sm text-gray-600">4chan's music board.</p>
+                <p class="text-sm text-gray-300">4chan's music board.</p>
             </a>
         </div>
     </section>
@@ -251,26 +269,26 @@
         >
             <a
                 href="https://www.reddit.com/r/fitness"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/r/fitness</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Subreddit for discussions about all things fitness.
                 </p>
             </a>
             <a
                 href="https://boards.4channel.org/fit/catalog"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">/fit/</h3>
-                <p class="text-sm text-gray-600">4chan's fitness board.</p>
+                <p class="text-sm text-gray-300">4chan's fitness board.</p>
             </a>
             <a
                 href="https://connect.garmin.com/modern/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Garmin</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Track, analyze, and share health and fitness activities from
                     your Garmin device.
                 </p>
@@ -285,30 +303,30 @@
         >
             <a
                 href="https://vault.bitwarden.com/#/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Bitwarden</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     The most trusted password manager for passwords and
                     passkeys.
                 </p>
             </a>
             <a
                 href="https://www.wikipedia.org"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Wikipedia</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     A free online encyclopedia, created and edited by volunteers
                     around the world.
                 </p>
             </a>
             <a
                 href="https://www.amazon.de/"
-                class="block p-4 bg-white rounded-lg shadow hover:bg-gray-50"
+                class="block p-4 bg-gray-900 rounded-lg shadow hover:bg-gray-800"
             >
                 <h3 class="text-lg font-semibold">Amazon</h3>
-                <p class="text-sm text-gray-600">
+                <p class="text-sm text-gray-300">
                     Low prices for electronics, books, sports equipment, and
                     more.
                 </p>
